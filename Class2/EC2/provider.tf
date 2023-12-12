0714d4e977267c0bd00e9232b7b0e6802ef4c671
kaizenacademy <126742188+kaizenacademy@users.noreply.github.com>
2023-12-12 17:11:40 -0600

    Update provider.tf

--- Class2/EC2/provider.tf
+++ Class2/EC2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA4SL6JVL7AWI4FWMO"
-  secret_key = "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"
+  access_key = "AKIA4SL6JVL7JDDARIKJ"
+  secret_key = "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"
 }
